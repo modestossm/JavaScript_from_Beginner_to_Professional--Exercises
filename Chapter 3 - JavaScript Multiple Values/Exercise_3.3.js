98af2970f93bdd4fca91db7ae2bb06eef2350e10
modestossm <modestossm@gmail.com>
2026-03-04 20:08:23 -0300

    Added: Exercise 3.3

---
+++ Chapter 3 - JavaScript Multiple Values/Exercise_3.3.js
@@ -0,0 +1,9 @@
+// 1. Create an array containing three values: 1, 2, and 3.
+// 2. Nest the original array into a new array three times.
+// 3. Output the value 2 from one of the arrays into the console.
+
+const arr = [1, 2, 3];
+
+const arrayOfArrays = [arr, arr, arr];
+
+console.log(arrayOfArrays[1][1]);
