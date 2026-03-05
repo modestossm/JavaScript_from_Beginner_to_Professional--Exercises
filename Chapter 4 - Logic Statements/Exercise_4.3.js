34663ba6fdc8bec9dac443c58b7c333abe0c7056
modestossm <modestossm@gmail.com>
2026-03-05 16:31:22 -0300

    Added: Exercise 4.3

---
+++ Chapter 4 - Logic Statements/Exercise_4.3.js
@@ -0,0 +1,9 @@
+// 1. Create a Boolean value for an ID variable
+// 2. Using a ternary operator, create a message variable that will check whether their ID is valid and either allow a person into a venue or not
+// 3. Output the response to the console
+
+let id = true;
+
+msg = id ? "ID is valid" : "ID is not valid";
+
+console.log(msg);
