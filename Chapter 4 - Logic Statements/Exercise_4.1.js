bcf87bab1b02c54d6aa5c84967dd300490dc5fee
modestossm <modestossm@gmail.com>
2026-03-05 15:42:32 -0300

    Added: Exercise 4.1

---
+++ Chapter 4 - Logic Statements/Exercise_4.1.js
@@ -0,0 +1,20 @@
+// 1. Create a variable with a Boolean value.
+// 2. Output the value of the variable to the console.
+// 3. Check whether the variable is true and if so, output a message to the console,
+// using the following syntax:
+// if(myVariable){
+//   //action
+// }
+// 4. Add another if statement with an ! in front of the variable to check whether the condition is not true, and create a message that will be printed to the
+// console in that instance. You should have two if statements, one with an ! and the other without. You could also use an if and an else statement instead—experiment!
+// 5. Change the variable to the opposite to see how the result changes.
+
+let var1 = true;
+
+console.log(var1);
+
+if(!var1){
+    console.log("It's true");
+} else {
+    console.log("It's not true");
+}
