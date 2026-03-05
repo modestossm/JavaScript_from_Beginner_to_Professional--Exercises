801abea30c798f179ae9d632244e322623469579
modestossm <modestossm@gmail.com>
2026-03-04 21:10:40 -0300

    Added: Exercise 3.5

---
+++ Chapter 3 - JavaScript Multiple Values/Exercise_3.5.js
@@ -0,0 +1,10 @@
+// 1. Create an object named people that contains an empty array that is called friends.
+// 2. Create three variables, each containing an object, that contain one of your friend's first names, last names, and an ID value.
+// 3. Add the three friends to the friend array.
+// 4. Output it to the console.
+
+let people = {
+    friends: ["friend 1", "friend 2", "friend 3"]
+};
+
+console.log(people.friends);
