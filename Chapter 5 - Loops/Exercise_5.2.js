8c76aaebaf299487e671520de15efec2035c8a4a
modestossm <modestossm@gmail.com>
2026-03-05 21:26:01 -0300

    Added: Exercise 5.2

---
+++ Chapter 5 - Loops/Exercise_5.2.js
@@ -0,0 +1,13 @@
+// 1. Set the starting counter to 0
+// 2. Create a variable, step, to increase your counter by
+// 3. Add a do while loop, printing the counter to the console and incrementing it by the step amount each loop
+// 4. Continue to loop until the counter is equal to 100 or more than 100
+
+let counter = 0;
+
+let step = 1;
+
+do {
+    console.log(counter);
+    counter += step;
+} while (counter <= 100);
