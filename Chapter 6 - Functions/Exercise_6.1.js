4ea59586f8ea74fba08a2c929bd53ad9a6d0e825
modestossm <modestossm@gmail.com>
2026-03-06 21:55:51 -0300

    Added: Exercise 6.1

---
+++ Chapter 6 - Functions/Exercise_6.1.js
@@ -0,0 +1,14 @@
+// 1. Create a function that takes two parameters, adds the parameters together, and returns the result.
+// 2. Set up two different variables with two different values.
+// 3. Use your function on the two variables, and output the result using console.log.
+// 4. Create a second call to the function using two more numbers as arguments sent to the function.
+
+function myFunction(param1, param2) {
+    return param1 + param2;
+}
+
+const var1 = 10;
+const var2 = 50;
+
+console.log(myFunction(var1, var2));
+console.log(myFunction(20, 70));
