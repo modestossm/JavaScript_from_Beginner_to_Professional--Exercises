427a1e76b0923c3816691ffd38003e4b77d902c0
modestossm <modestossm@gmail.com>
2026-03-09 21:59:48 -0300

    Added: Exercise 6.6

---
+++ Chapter 6 - Functions/Exercise_6.6.js
@@ -0,0 +1,20 @@
+// 1. Create a function that contains a condition within it checking if the argument value is 0.
+// 2. If the parameter is equal to 0, it should return the value of 1. Otherwise, it should return the value of the argument multiplied by the value returned from the function itself, subtracting one from the value of the argument that is provided. This will result in running the block of code until the value reaches 0.
+// 3. Invoke the function, providing an argument of whatever number you want to find the factorial of. The code should run whatever number is passed initially into the function, decreasing all the way to 0 and outputting the results of the calculation to the console. It could also contain a console.log() call to print the current value of the argument in the function as it gets invoked.
+// 4. Change and update the number to see how it affects the results.
+
+function verifyFactorial(arg) {
+    if(arg === 0) {
+        let result;
+        result = arg * verifyFactorial(arg-1);
+        console.log(arg + "! = " + result);
+        return result
+        
+    } else if(arg === 0) {
+        return 1;
+    } else {
+        return console.log("Error: the argument needs to be greater than 0.");
+    };
+}
+
+verifyFactorial(7);
