f704a7a8f3e9168d0d2eb61ccffe4a1320b6a6ab
modestossm <modestossm@gmail.com>
2026-03-11 22:13:28 -0300

    Added: Exercise 7.3

---
+++ Chapter 7 - Classes/Exercise_7.3.js
@@ -0,0 +1,26 @@
+// 1. Create a method that prints a given animal and its sound.
+// 2. Add a prototype with another action for the animal.
+// 3. Output an entire animal object into the console.
+
+class Animal {
+    constructor(tipo, sound, velocity) {
+        this.tipo = tipo;
+        this.sound = sound;
+        this.velocity = velocity;
+    }
+
+    callAnimalSound() {
+        console.log("The " + this.tipo + " sounds like: " + this.sound);
+    }
+}
+
+
+Animal.prototype.callAnimalVelocity = function() {
+    console.log("The " + dog.tipo + " runs at: " + dog.velocity);
+}
+
+let dog = new Animal("dog", "au au", "50 km/h");
+
+console.log(dog);
+dog.callAnimalSound();
+dog.callAnimalVelocity();
