3e8300fe092fbc6f8b7e79e40840e8cfc1d731fe
modestossm <modestossm@gmail.com>
2026-03-17 18:36:02 -0300

    Added: Exercise 10.2

---
+++ Chapter 10 - Dynamic Element Manipulation Using the DOM/Exercise_10.2.js
@@ -0,0 +1,8 @@
+// Try experimenting with getting elements by their IDs:
+
+// 1. Create an HTML element and assign an ID in the element attribute.
+// 2. Select the page element using its ID.
+// 3. Output the selected page element into the console.
+
+let element = document.getElementById("title");
+console.log(element);
