b8226cd5d7116bfab8a7ff8d8b521e2141a55c23
modestossm <modestossm@gmail.com>
2026-03-17 19:10:52 -0300

    Added: Exercise 10.3

---
+++ Chapter 10 - Dynamic Element Manipulation Using the DOM/Exercise_10.3.js
@@ -0,0 +1,11 @@
+// Use JavaScript to select page elements via their tag name:
+
+// 1. Start by creating a simple HTML file.
+// 2. Create three HTML elements using the same tag.
+// 3. Add some content within each element so you can distinguish between them
+// 4. Add a script element to your HTML file, and within it select the page elements by tag name and store them in a variable as an array
+// 5. Using the index value, select the middle element and output it into the console.
+
+let elements = document.getElementsByTagName("div");
+
+console.log(elements[1].innerHTML);
