84c105efae6e97add7d7d1a024a9e957586e497a
modestossm <modestossm@gmail.com>
2026-03-21 21:41:55 -0300

    Added: Exercise 12.2

---
+++ Chapter 12 - Intermediate JavaScript/Exercise_12.2.js
@@ -0,0 +1,22 @@
+const output = document.querySelector(".output");
+const emailVal = document.querySelector("input");
+const btn = document.querySelector("button");
+
+const emailExp =/([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
+
+btn.addEventListener("click", (e) => {
+    const val = emailVal.value;
+    const result = emailExp.test(val);
+    let response = "";
+
+    if (!result) {
+        response = "Invalid Email";
+        output.style.color = "red";
+    } else {
+        response = "Valid Email";
+        output.style.color = "green";
+    }
+    
+    emailVal.value = "";
+    output.textContent = response;
+});
