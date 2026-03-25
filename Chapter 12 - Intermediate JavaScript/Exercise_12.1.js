593f68060aaa35361ffe7f3a5786ef4574f33cd0
modestossm <modestossm@gmail.com>
2026-03-25 19:46:38 -0300

    Added: Exercise 13.1

--- Chapter 12 - Intermediate JavaScript/Exercise_12.1.js
+++ Chapter 12 - Intermediate JavaScript/Exercise_12.1.js
@@ -1,3 +1,13 @@
+// Find and replace strings. The following exercise involves replacing characters in a specified string value. 
+// The first input field will indicate which character string will be replaced, and the second input field will indicate which characters will replace them once the button is clicked.
+
+// 1. Select each of the three page elements using JavaScript and assign the element objects as variables so that they can be easily referenced in your code.
+// 2. Add an event listener to the button to invoke a function when clicked.
+// 3. Create a function named lookup() that will find and replace the text in the output element. Assign the output element's text content to a variable named s, and then assign the value of the input we are replacing to another variable named rt.
+// 4. Create a new regex with the value of the first input field, which will allow you to replace the text. Using the regex, check for a match with the match() method. Wrap this with a condition that will execute a block of code if matches are found.
+// 5. If the match is found, use replace() to set the new value.
+// 6. Update the output area with the newly created and updated text output.
+
 const output = document.getElementById("output");
 const findValue = document.getElementById("sText");
 const replaceValue = document.getElementById("rText");
--- Chapter 12 - Intermediate JavaScript/Exercise_12.2.js
+++ Chapter 12 - Intermediate JavaScript/Exercise_12.2.js
@@ -1,3 +1,11 @@
+// Create an application that uses JavaScript to check whether the string value of an input is a validly formatted email using regex.
+
+// 1. Use the above template code to start creating your application. Within the JavaScript code, select the input, output, and button elements from the page as JavaScript objects.
+// 2. Add an event listener to the button to run a block of code when clicked that will get the current value in the input field. Create a blank response value that will populate the output div element contents.
+// 3. Add a test with the string value from the input field and the expression for email format. If the test result is false, update the response output to say Invalid Email and change the output color to red.
+// 4. If the condition of the test returns true, add a response that confirms the email format is correct and change the text color of output to green.
+// 5. Output the response value into the output element.
+
 const output = document.querySelector(".output");
 const emailVal = document.querySelector("input");
 const btn = document.querySelector("button");
